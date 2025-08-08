60cfa7f9487acc04ad87324ecf3826aa0e1128ef
Joseph Portas <joeportas05@gmail.com>
2025-08-08 02:06:58 -0400

    5.3

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
+import "dotenv/config";
 
 import express from "express"; 
 import UserRoutes from "./Kambaz/Users/routes.js";
 
-const app = express();
-UserRoutes(app);
+import session from "express-session";
 
-app.use(cors());
+import CourseRoutes from "./Kambaz/Courses/routes.js";
+
+import ModuleRoutes from "./Kambaz/Modules/routes.js";
+
+const app = express();
+
+app.use(cors(cors({
+credentials: true,
+origin: process.env.NETLIFY_URL || "http://localhost:5173",
+})));
+const sessionOptions = {
+secret: process.env.SESSION_SECRET || "kambaz",
+resave: false,
+saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+sessionOptions.proxy = true;
+sessionOptions.cookie = {
+sameSite: "none",
+secure: true,
+domain: process.env.NODE_SERVER_DOMAIN,
+};
+}
+app.use(session(sessionOptions));
 app.use(express.json());
-Lab5(app);
+UserRoutes(app);
+CourseRoutes(app);
+EnrollmentRoutes(app);
+ModuleRoutes(app);
 Hello(app);
+Lab5(app);
 app.listen(process.env.PORT || 4000)
